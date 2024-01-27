29125ad74965e79a12e58498665a619a2fbdc77d
ilias <iliasayman14@gmail.com>
2024-01-27 23:15:40 +0100

    Spring boot project

---
+++ src/main/java/com/InvoicifyPro/InvoicifyPro/InvoicifyProApplication.java
@@ -0,0 +1,13 @@
+package com.InvoicifyPro.InvoicifyPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicifyProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicifyProApplication.class, args);
+	}
+
+}
