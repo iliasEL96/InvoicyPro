8cbcb594556e9b152acc49f71ef887ec695c863e
ilias <iliasayman14@gmail.com>
2024-03-17 17:19:27 +0100

    L'ajout du controller de l'entité Client

---
+++ src/main/java/com/InvoicifyPro/InvoicifyPro/controller/ClientController.java
@@ -0,0 +1,38 @@
+package com.InvoicifyPro.InvoicifyPro.controller;
+
+import com.InvoicifyPro.InvoicifyPro.dto.dtoEntite.ClientDTO;
+import com.InvoicifyPro.InvoicifyPro.service.client.ClientService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/client")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+    @GetMapping
+    public List<ClientDTO> getAllClient(){
+        return clientService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ClientDTO getClientByID( @PathVariable Long id){
+        return clientService.findById(id);
+    }
+
+    @PostMapping
+    public ClientDTO createClient(@RequestBody ClientDTO clientDTO ){
+        return clientService.save(clientDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById( @PathVariable Long id){
+         clientService.deleteById(id);
+    }
+}
