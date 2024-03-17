148bdaccd9d358e8b7ff02f24a29d1675edd8c97
ilias <iliasayman14@gmail.com>
2024-03-17 17:32:34 +0100

    L'ajout du controller de l'entité Expense

---
+++ src/main/java/com/InvoicifyPro/InvoicifyPro/controller/ExpenseController.java
@@ -0,0 +1,42 @@
+package com.InvoicifyPro.InvoicifyPro.controller;
+
+
+import com.InvoicifyPro.InvoicifyPro.dto.dtoEntite.ExpenseDTO;
+import com.InvoicifyPro.InvoicifyPro.service.expense.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/expense")
+public class ExpenseController {
+
+    private final ExpenseService expenseService;
+
+    @Autowired
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @GetMapping
+    public List<ExpenseDTO> getAllExpense(){
+        return expenseService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ExpenseDTO getExpenseById(@PathVariable Long id){
+        return expenseService.findById(id);
+    }
+
+    @PostMapping
+    public ExpenseDTO createExpense(@RequestBody ExpenseDTO expenseDTO){
+        return expenseService.save(expenseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteExpense(@PathVariable Long id){
+        expenseService.deleteById(id);
+    }
+}
